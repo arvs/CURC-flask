489c5882535c5ba06f05ae1718df1c93dfad393b
Arvind <arvind@rumney>
2012-09-07 12:32:51 +0000

    modify params before inserting, add cap on number of mphc memberships

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request, jsonify
+from werkzeug.contrib.fixers import ProxyFix
 import requests
 from db_logic import DBConnection
 app = Flask(__name__)
@@ -6,17 +7,28 @@ app.debug = True
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	db = DBConnection()
+	num_members = db.get('members',gym='mphc')
+	if len(num_members) >= 50:
+		return render_template('index.html', mphc_available=False)
+	return render_template('index.html', mphc_available=True)
 
 @app.route('/registration.json')
 def register():
 	db = DBConnection()
-	params = request.args
+	d = request.args
+	params = {k:v for k,v in d.iteritems()}
+	params['gender'] = params.pop('gensel')
+	params['driver'] = True if params.pop('dsel') is 'y' else False
+	params['graduating'] = True if params.pop('gsel') is 'y' else False
+	params['shirt'] = params.pop('ssel')
+	params['gym'] = params.pop('msel')
 	if len(db.get('members', fname=params['fname'], lname=params['lname'])) == 0:
 		db.insert('members', **params)
 		return jsonify(already_inserted='false')
 	else:
 		return jsonify(already_inserted='true')
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
 	app.run()
