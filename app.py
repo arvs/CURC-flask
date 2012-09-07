e424ce2725dc9f15864364b101825a67b59dfab2
Arvind Srinivasan <arvind.srinivasan@gmail.com>
2012-09-07 05:15:22 -0400

    static site complete

--- app.py
+++ app.py
@@ -6,62 +6,14 @@ app.debug = True
 
 @app.route('/')
 def index():
-	return render_template('index.html', profs=profs, js_url=url_for("static",filename="search_cu.js"))
+	return render_template('index.html')
 
 @app.route('/search_for_prof')
 def search_for_prof():
 	db = DBConnection()
 	name = request.args.get("name","")
-	classes = db.classes_for_name(name)
-	if classes is not None:
-		return jsonify(name=name, classes=classes)
-	params = {
-			  "q" : name, 
-			  "site" : "Directory_of_Classes", 
-			  "num" : 20, 
-			  "filter" : 0,
-			  "entqr" : 0, 
-			  "ud" : 1,
-			  "sort" : "date%3AD%3AL%3Ad1", 
-			  "output" : "xml_no_dtd",
-			  "oe" : "UTF-8",
-			  "ie" : "UTF-8",
-			  "client" : "DoC",
-			  "proxystylesheet" : "DoC",
-			  "proxyreload" : 1
-			 }
-	search = requests.get("http://search.columbia.edu/search", params=params)
-	search_result = BeautifulSoup(search.content)
-	if search_result.find(id="empty") is None:
-		classes = []
-		for cls in search_result.find_all("a","l"):
-			if "Fall 2012" in "".join(map(str, cls.contents)):
-				x = cls.get('href')
-				classes.append(x)
-				db.insert('classes', Instructor = name, link=x)
-		return jsonify(name=name, classes=classes)
-	db.insert('classes', Instructor = name, no_classes=True)
+	db.insert('classes', Instructor = name, link=x)
 	return jsonify(name=name, classes=[])
-
-@app.route('/populate_class')
-def populate_class():
-	db = DBConnection()
-	name = request.args.get("name","")
-	links = [x['link'] for x in db.get("classes", Instructor=name)]
-	for link in links:
-		r = requests.get(link)
-		soup = BeautifulSoup(r.content)
-		print "processed, %s" % (r.status_code) 
-		rows = list(soup.find_all('tr'))[1:]
-		rowdict = {}
-		for row in rows[1:]:
-			row = filter(lambda x: x != '\n', row)
-			if len(row) == 2:
-				rowdict.update({row[0].get_text().strip() : row[1].get_text().strip()})
-		print rowdict
-		db.update("classes", {'Instructor': name, 'link':link},**rowdict)
-
-	return jsonify(links=links)
 
 if __name__ == '__main__':
 	app.run()
